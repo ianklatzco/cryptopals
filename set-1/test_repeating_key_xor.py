93dcc2cccf45e63869e8cbeb64a97c5615556ff9
Ian Klatzco <iklatzco@gmail.com>
2020-03-29 12:51:54 -0700

    repeating key xor

---
+++ set-1/test_repeating_key_xor.py
@@ -0,0 +1,10 @@
+from repeating_key_xor import *
+
+def test_repeating_key_xor():
+	plaintext = bytes('Burning \'em, if you ain\'t quick and nimble\nI go crazy when I hear a cymbal','ascii')
+	key = bytes('ICE','ascii')
+
+	goal_ciphertext = bytes.fromhex('0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f')
+
+	assert repeating_key_xor(plaintext, key) == goal_ciphertext
+
